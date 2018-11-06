673fef8a17f5718333e812eb7d7520254ff0a2ef
Rick <42774042+Shrugged2@users.noreply.github.com>
2018-11-06 01:25:56 -0600

    Add files via upload

--- ENGL1301.py
+++ ENGL1301.py
@@ -14,6 +14,8 @@ Primary = input("primary argument: ")
 Secondary = input("secondary argument: ")
 Support1 = input("Support for primary arguement: ")
 Support2 = input("Support for secondary argument: ")
+Weakness = input("Weaknesses of essay: ")
+Strength = input("Strength of essay: ")
 # placeholder input because I'm sure I'll think of more
 #  = input("")
 
@@ -29,22 +31,43 @@ print (now.month, now.day, now.year)
 print ("Essay #")
 
 #Intro PPH.
-print ("In the essay",Title,"by", AuthorFN, AuthorLN,",",AuthorLN, "explains")
+print ("In the essay",Title,"by", AuthorFN, AuthorLN,",",AuthorLN, "explains", end = ' ')
 if Gender == "M":
-    print ("his belief that",)
+    print (" his belief that", end = ' ')
 else:
-    print ("her belief that")
+    print (" her belief that", end = ' ')
 
 
 
-print (AuthorThesis,".")
+print (AuthorThesis, end = '. ')
 
 if Gender == "M":
-    print("He argues that",)
+    print(" He argues that",end = ' ')
 else:
-    print ("She argues that")
+    print (" She argues that", end = ' ')
 #THesis
-print (SubjectOE, "has", Impact,".",
-    AuthorLN,"'s primary argument states that", Primary,".",AuthorLN,"s secondary argument is that",
-    Secondary, Support2,".")
-print (AuthorLN, "s main point is that", RestateThesis,RestateSupport,". Although this essay")
+print (SubjectOE, "has", Impact,". ", AuthorLN,"'s primary argument states that", Primary,".",AuthorLN,"s secondary argument is that",
+    Secondary, Support2, end = '. ')
+print (" ",AuthorLN, "s main point is that", RestateThesis,RestateSupport,".","Although this essay",Weakness,", the author", Strength)
+
+#Paragraph 2
+
+
+#Paragraph 3
+
+#Conclusion
+print (AuthorLN, "was successful in presenting and persuading",end = ' ')
+
+if Gender == "M":
+    print(" his audience of",end = ' ')
+else:
+    print (" her audience of", end = ' ')
+
+print (SubjectOE,".",end = '.') #Tie back to original subject
+
+if Gender == "M":
+    print(" His arguments",end = ' ')
+else:
+    print (" Her arguments", end = ' ')
+
+print ("served to effectively convey the message intended.", )
